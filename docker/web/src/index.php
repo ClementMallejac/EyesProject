b291fe54e8d04cc4a8cce0923fb833649137d301
Adrien Bonnamour <adrien@bonnamour.me>
2019-09-24 16:09:11 +0200

    Ajout du serveur apache + premiere homepage

---
+++ docker/web/src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <title>Eyes Project - Bienvenue</title>
+</head>
+<body>
+    <div class='main home'>
+        <div class='home__wrapper'>
+            <picture class='home__picture home__picture--logo'>
+                <img class='home__image home__image--logo' src="img/home-logo-eyesproject.jpg" alt="Home Logo Eyes Project">
+            </picture>
+            <h1 class='home__title'>Eyes Project</h1>
+            <p class='home__presentation'>EyesProject est un outil permettant de recuperer, enregistrer et traiter le regard de personne sur des images.</p>
+            <a class='home__button button button--large' href="/new-session">Commencer l'experience</a>
+        </div>
+    </div>
+</body>
+</html>
