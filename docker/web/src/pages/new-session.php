41f4625ec96d9a12cc5c10af367dc8dfebcd3ead
Adrien Bonnamour <adrien@bonnamour.me>
2019-10-18 19:47:41 +0200

    (WebGazer) Ajout du callibrage de l'EyeTracking

---
+++ docker/web/src/pages/new-session.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <title>Eyes Project - Nouvelle session</title>
+</head>
+<body>
+    <div class='barre'>
+        <picture class='picture picture--logo'>
+            <img class='image image--logo' src="img/logo-eyesproject.jpg" alt="Logo Eyes Project">
+        </picture>
+    </div>
+    <div class='main'>
+        <div class='formulaire'>
+            <form action="">
+                <input type="text">
+            </form>
+            <button type="submit" form="form1" value="Envoyer">
+        </div>
+    </div>
+</body>
+</html>
