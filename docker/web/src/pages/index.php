c7eb79cd784a7ff57d8490bb6ea634847f322a5a
Adrien Bonnamour <adrien@bonnamour.me>
2019-10-18 22:27:58 +0200

    Debut formulaire

--- docker/web/src/pages/index.php
+++ docker/web/src/pages/index.php
@@ -12,11 +12,11 @@
     <div class='main home'>
         <div class='home__wrapper'>
             <picture class='home__picture home__picture--logo'>
-                <img class='home__image home__image--logo' src="img/home-logo-eyesproject.jpg" alt="Home Logo Eyes Project">
+                <img class='home__image home__image--logo' src="img/logo-eyesproject-big.jpg" alt="Home Logo Eyes Project">
             </picture>
             <h1 class='home__title'>Eyes Project</h1>
             <p class='home__presentation'>EyesProject est un outil permettant de recuperer, enregistrer et traiter le regard de personne sur des images.</p>
-            <a class='home__button button button--large' href="/callibration">Commencer l'experience</a>
+            <a class='home__button button button--large' href="/new-session">Commencer l'experience</a>
         </div>
     </div>
 </body>
--- docker/web/src/pages/new-session.php
+++ docker/web/src/pages/new-session.php
@@ -1,26 +1,66 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <link rel="stylesheet" type="text/css" href="css/new-session.css">
     <title>Eyes Project - Nouvelle session</title>
 </head>
+
 <body>
     <div class='barre'>
         <picture class='picture picture--logo'>
-            <img class='image image--logo' src="img/logo-eyesproject.jpg" alt="Logo Eyes Project">
+            <img class='image image--logo' src="img/logo-eyesproject-small.jpg" alt="Logo Eyes Project">
         </picture>
     </div>
     <div class='main'>
         <div class='formulaire'>
             <form action="">
-                <input type="text">
+                <fieldset>
+                    <div class='formulaire__row'>
+                        <div class='formulaire__element'>
+                            <label class='formulaire__label' for='form_email'>Email</label>
+                            <input class='formulaire__input' id='form_email' name='email' type="email" required>
+                        </div>
+                    </div>
+                    <div class='formulaire__row'>
+                        <div class='formulaire__element'>
+                            <label class='formulaire__label' for='form_firstname'>Prenom</label>
+                            <input class='formulaire__input' id='form_firstname' name='firstname' type="text" required>
+                        </div>
+                    </div>
+                    <div class='formulaire__row'>
+                        <div class='formulaire__element'>
+                            <label class='formulaire__label' for='form_lastname'>Nom</label>
+                            <input class='formulaire__input' id='form_lastname' name='lastname' type="text" required>
+                        </div>
+                    </div>
+
+                    <div class='formulaire__row'>
+                        <div class='formulaire__element'>
+                            <label class='formulaire__label' for='form_age'>Age</label>
+                            <input class='formulaire__input' id='form_age' name='age' type="text" required>
+                        </div>
+                    </div>
+                    <div class='formulaire__row'>
+                        <label class='formulaire__label' for='form_sexe'>Sexe</label>
+                        <div class='formulaire__element formulaire__element--choice'>
+                            <input class='formulaire__input formulaire__input--radio' id='form_male' name='sexe' type="radio">
+                            <label class='formulaire__label' for='form_male'>Masculin</label>
+                        </div>
+                        <div class='formulaire__element formulaire__element--choice'>
+                            <input class='formulaire__input formulaire__input--radio' id='form_female' name='sexe' type="radio">
+                            <label class='formulaire__label' for='form_female'>Feminin</label>
+                        </div>
+                    </div>
+                    <button type="submit" form="form1">Envoyer</button>
+                </fieldset>
             </form>
-            <button type="submit" form="form1" value="Envoyer">
         </div>
     </div>
 </body>
+
 </html>
